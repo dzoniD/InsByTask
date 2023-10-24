aa8a91841ae04b7d72ab456b880e073b0b7831cf
dzoniD <edvard_dantes@hotmail.com>
2023-10-24 23:35:46 +0200

    created auth context

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 import { TokenContextProvider } from "@/context/TokenContext";
+import { AuthProvider } from "@/context/AuthContext";
 
 const sourceSans3 = Source_Sans_3({ subsets: ["latin"] });
 
@@ -13,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${sourceSans3.className} `}>
-        <TokenContextProvider>
-          <div className="max-w-[1536px] mx-auto">{children}</div>
-        </TokenContextProvider>
+        <AuthProvider>
+          <TokenContextProvider>
+            <div className="max-w-[1536px] mx-auto">{children}</div>
+          </TokenContextProvider>
+        </AuthProvider>
       </body>
     </html>
   );
